15f875a98531245d2ee9491f2601742cccb079de
brettcox <brettcoxemail@yahoo.com>
2015-12-23 21:02:23 -0600

    First commit
    Added all the first files, kinda!

---
+++ src/main/java/io/github/brettcox/minecraftminigames/MainClass.java
@@ -0,0 +1,15 @@
+package io.github.brettcox.minecraftminigames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MainClass extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
